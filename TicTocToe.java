de455f08af397369fde7337fece96442e761df69
Sheetal <87216418+shitals1202@users.noreply.github.com>
2021-09-02 18:35:24 +0530

    Update Code changes

--- TicTocToe.java
+++ TicTocToe.java
@@ -8,10 +8,6 @@ public class TicTocToe {
 	static String[] board;
 	static String turn;
 	
-	
-	// CheckWinner method will
-	// decide the combination
-	// of three box given below.
 	static String checkWinner()
 	{
 		for (int a = 0; a < 8; a++) {
@@ -43,12 +39,9 @@ public class TicTocToe {
 				line = board[2] + board[4] + board[6];
 				break;
 			}
-			//For X winner
 			if (line.equals("XXX")) {
 				return "X";
 			}
-			
-			// For O winner
 			else if (line.equals("OOO")) {
 				return "O";
 			}
@@ -63,22 +56,11 @@ public class TicTocToe {
 				return "draw";
 			}
 		}
-
-	// To enter the X Or O at the exact place on board.
 		System.out.println(
 			turn + "'s turn; enter a slot number to place "
 			+ turn + " in:");
 		return null;
 	}
-	
-	// To print out the board.
-	/* |---|---|---|
-	| 1 | 2 | 3 |
-	|-----------|
-	| 4 | 5 | 6 |
-	|-----------|
-	| 7 | 8 | 9 |
-	|---|---|---|*/
 	
 	static void printBoard()
 	{
@@ -117,10 +99,6 @@ public class TicTocToe {
 		while (winner == null) {
 			int numInput;
 			
-		// Exception handling.
-		// numInput will take input from user like from 1 to 9.
-		// If it is not in range from 1 to 9.
-		// then it will show you an error "Invalid input."
 			try {
 				numInput = in.nextInt();
 				if (!(numInput > 0 && numInput <= 9)) {
@@ -135,8 +113,6 @@ public class TicTocToe {
 				continue;
 			}
 			
-			// This game has two player x and O.
-			// Here is the logic to decide the turn.
 			if (board[numInput - 1].equals(
 					String.valueOf(numInput))) {
 				board[numInput - 1] = turn;
@@ -156,9 +132,6 @@ public class TicTocToe {
 					"Slot already taken; re-enter slot number:");
 			}
 		}
-		
-		// If no one win or lose from both player x and O.
-		// then here is the logic to print "draw".
 		if (winner.equalsIgnoreCase("draw")) {
 			System.out.println(
 				"It's a draw! Thanks for playing.");
